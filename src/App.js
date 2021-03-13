70476fad879e35f64ffaf1edeb19b1d3291ac157
PieterT2000 <info@codebirds.nl>
2021-03-13 17:27:30 +0000

    add OSM map with WFS layer config

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import React, { useState } from "react";
+
+import MapWrapper from "./Map/MapWrapper";
+import { GeoJSON } from "ol/format";
 
 function App() {
+  const [woonplaats, setWoonplaats] = useState("");
+  const [features, setFeatures] = useState("");
+
+  let api =
+    "https://geodata.nationaalgeoregister.nl/bag/wfs/v1_1?service=WFS&version=2.0.0&request=GetFeature&typename=bag:woonplaats&count=5&outputFormat=json&count-5";
+
+  const handleInput = (e) => {
+    let val = e.target.value;
+    // Capitalize woonplaats
+    val = val.charAt(0).toUpperCase() + val.slice(1);
+    setWoonplaats(val);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!woonplaats) return;
+
+    const queryParam = "&filter=";
+    const filterStr = `<Filter><PropertyIsEqualTo><PropertyName>bag:woonplaats</PropertyName><Literal>${woonplaats}</Literal></PropertyIsEqualTo></Filter>`;
+    api += queryParam + encodeURIComponent(filterStr);
+
+    // Fetch new vector source
+    fetch(api)
+      .then((res) => res.json())
+      .then((fetchedFeatures) => {
+        // Parse Features from return JSON object
+        const parsedFeatures = new GeoJSON().readFeatures(fetchedFeatures);
+
+        // Pass features on to MapWrapper component (state value gets passed a prop)
+        setFeatures(parsedFeatures);
+      });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Test map!</h1>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        <input
+          onChange={(e) => handleInput(e)}
+          type="text"
+          autoComplete="off"
+        />
+        <button>Submit</button>
+      </form>
+      <MapWrapper features={features} />
     </div>
   );
 }
